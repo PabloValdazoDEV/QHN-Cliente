609b67b4da79bfb8d5acddaf9f131c1a138ef9b0
PabloValdazoDEV <157512231+PabloValdazoDEV@users.noreply.github.com>
2025-04-26 21:19:46 +0200

    feat: correciones puntuales

--- src/Components/Cards/CardVerticalMini.jsx
+++ src/Components/Cards/CardVerticalMini.jsx
@@ -2,7 +2,7 @@ export default function CardVerticalMini({ title, description, image, link }) {
     return (
       <a
         href={link || '#'}
-        className="block group transition-soft rounded-base overflow-hidden bg-white shadow-md hover:shadow-lg dark:bg-neutral-900 rounded-lg"
+        className="block group transition-soft rounded-base overflow-hidden bg-white shadow-md hover:shadow-lg dark:bg-neutral-900 rounded-lg no-underline"
       >
         <img
           src={image}
@@ -10,7 +10,7 @@ export default function CardVerticalMini({ title, description, image, link }) {
           className="w-full h-30 object-cover transition-transform duration-300 group-hover:scale-105"
         />
         <div className="p-3">
-          <h3 className="text-lg font-display font-semibold text-[color:var(--color-primary)] group-hover:underline mb-1 dark:text-neutral-300">
+          <h3 className="text-lg font-display font-semibold text-[color:var(--color-primary)]  mb-1 dark:text-neutral-300">
             {title}
           </h3>
           <p className="text-xs text-neutral-700 dark:text-neutral-300 rounded-lg">
