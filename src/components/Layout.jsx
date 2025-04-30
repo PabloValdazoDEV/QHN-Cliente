cd3184b204d94f152145ee96dfb06e85f6010eda
PabloValdazoDEV <157512231+PabloValdazoDEV@users.noreply.github.com>
2025-04-30 08:44:49 +0200

    feat: banner y logia de cookies

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -2,11 +2,14 @@ import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Navbar from './Navbar';
 import Footer from './Footer';
+import BannerCookies from './Modales/BannerCookies';
 
 const Layout = () => {
+  
   return (
     <div className="min-h-screen flex flex-col bg-gray-100">
       <Navbar />
+      <BannerCookies/>
       <main className="flex-grow container mx-auto px-4 py-8 max-w-6xl">
         <Outlet />
       </main>
