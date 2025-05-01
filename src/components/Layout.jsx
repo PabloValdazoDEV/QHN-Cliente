cbcfe3b8563cf6b33ecfb8e400880416a605e189
LuisPS-coder <lps_1992@hotmail.com>
2025-05-01 20:37:54 +0200

    Feat: Modal de encuesta de datos añadido

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -3,19 +3,20 @@ import { Outlet } from 'react-router-dom';
 import Navbar from './Navbar';
 import Footer from './Footer';
 import BannerCookies from './Modales/BannerCookies';
+import RecommendModal from './Modales/RecommendationsForm'; 
 
 const Layout = () => {
-  
   return (
     <div className="min-h-screen flex flex-col bg-gray-100">
       <Navbar />
-      <BannerCookies/>
+      <BannerCookies />
       <main className="flex-grow container mx-auto px-4 py-8 max-w-6xl">
         <Outlet />
       </main>
-      <Footer/>
+      <Footer />
+      <RecommendModal /> 
     </div>
   );
 };
 
-export default Layout; 
+export default Layout;
